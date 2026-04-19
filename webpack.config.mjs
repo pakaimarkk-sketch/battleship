e36f126fe5ae62db73b6aa43f9187ef907c957d2
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-04-19 16:44:55 +0200

    initial commit

---
+++ webpack.config.mjs
@@ -0,0 +1,46 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+  entry: "./src/battleship/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/battleship/",
+    clean: true,
+  },
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.m?js$/,
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.svg$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
