8a64268c0f8fcba93b5c079d73b93d5ef3c82e9c
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-05-02 17:31:30 +0200

    feat: defined modes

---
+++ src/battleship/game/modes/gameModes.js
@@ -0,0 +1,5 @@
+export const GAME_MODES = {
+  CLASSIC: "classic",
+  MODERN: "modern",
+  CUSTOM: "custom",
+};
---
+++ src/battleship/game/modes/playerModes.js
@@ -0,0 +1,5 @@
+export const PLAYER_MODES = {
+  SINGLE_PLAYER: "singlePlayer",
+  LOCAL: "local",
+  MULTIPLAYER: "multiplayer",
+};
