e42ce66270f6febab11b8a17e083954196898758
pakaimarkk <251377257+pakaimarkk-sketch@users.noreply.github.com>
2026-05-25 13:38:10 +0200

    fix: import fix

--- src/battleship/game/config/bot/easyBotLogic.js
+++ src/battleship/game/config/bot/easyBotLogic.js
@@ -1,4 +1,4 @@
-import { randomPlacement } from "./strategies/botPlacementStrategies.js;";
+import { randomPlacement } from "./strategies/botPlacementStrategies.js";
 import { strategyRandomAttack } from "./strategies/botAttackStrategies.js";
 
 class EasyBotLogic {
